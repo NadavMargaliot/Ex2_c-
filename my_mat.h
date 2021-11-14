7b39899ea4a17d8b1b6088dd498991a15b243245
Adiel1892 <adiel1892@gmail.com>
2021-11-14 16:17:21 +0200

    main.c , Makefile , my_mat.h

---
+++ my_mat.h
@@ -0,0 +1,5 @@
+
+void fillMatrix(int mat[10][10]);
+void takeShortestPath(int minDestination[10][10]);
+void isTherePath(int mat[10][10], int i, int j);
+void printShortestPath(int mat[10][10],int i, int j);
