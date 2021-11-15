120ff5886aa3abaed35e0cfe9526b79166857925
Adiel1892 <adiel1892@gmail.com>
2021-11-15 11:18:04 +0200

    library

--- my_mat.h
+++ my_mat.h
@@ -1,6 +1,6 @@
 
-void fillMatrixFromInput(int mat[10][10]);
-void copyMatrix(int mat[10][10] , int copied[10][10]);
-void takeShortestPath(int minDestination[10][10]);
-int isTherePath(int mat[10][10], int i, int j);
-int shortestPath(int mat[10][10],int i, int j);
+void fillMatrixFromInput(int[10][10]);
+void copyMatrix(int[10][10] , int[10][10]);
+void takeShortestPath(int[10][10]);
+int isTherePath(int[10][10], int i, int j);
+int shortestPath(int[10][10],int i, int j);
