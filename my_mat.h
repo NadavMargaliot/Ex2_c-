ea3f0c4b6df82d3290579fb73f3c0f7041a10633
Adiel1892 <adiel1892@gmail.com>
2021-11-19 13:27:21 +0200

    delete min

--- my_mat.h
+++ my_mat.h
@@ -4,7 +4,6 @@
 void fillMatrixFromInput(int[size][size]);
 void copyMatrix(int[size][size] , int[size][size]);
 void takeShortestPath(int[size][size]);
-int min(int, int);
 int isTherePath(int[size][size]);
 int shortestPath(int[size][size]);
 
